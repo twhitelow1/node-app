69d7bfa4b7a9145cc3e0d5e77db694ae8a9c1cfe
Todd Whitelow <todd@whitelow.tech>
2020-09-20 17:28:31 -0600

    add mongo dd connection

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose',)
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+var dbUrl = 'mongodb+srv://ras-20:E1G2QzIRaxtQMG21@nodechat.i95u3.mongodb.net/nodechat?retryWrites=true&w=majority'
+
+var Message = mongoose.model('Message', {
+  name: String,
+  message: String
+})
 
 var messages = [
   { name: "Tim", message: "Hi!" },
@@ -18,6 +26,7 @@ app.get('/messages', (req, res) => {
 })
 
 app.post('/messages', (req, res) => {
+  var message = new Message(req.body)
   messages.push(req.body)
   io.emit('message', req.body) //event called message and passes req.body which contains the msg
   res.sendStatus(200)
@@ -25,6 +34,10 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
+  console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => console.log('Server is listening on port', server.address().port))
