7b711672eebae040b04e690bd4375e8ffd0ae957
Todd Whitelow <todd@whitelow.tech>
2020-09-20 14:39:06 -0600

    add post path

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ var app = express()
 app.use(express.static(__dirname))
 
 var messages = [
-  {
-    name: "Tim",
-    message: "Hi!"
-  },
-  {
-    name: "Todd",
-    message: "Howdy, Tim!"
-  }
+  { name: "Tim", message: "Hi!" },
+  { name: "Todd", message: "Howdy, Tim!" }
 ]
 
 app.get('/messages', (req, res) => {
   res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+  console.log(req.body)
+  res.sendStatus(200)
 })
 
 var server = app.listen(3000, () => console.log('Server is listening on port', server.address().port))
