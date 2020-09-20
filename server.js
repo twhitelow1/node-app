d6a0c93b4c0d0570a9c8556df0d0e6b9a7f820fb
Todd Whitelow <todd@whitelow.tech>
2020-09-20 14:24:12 -0600

    add get request for /messages

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(__dirname))
+
+app.get('/messages', (req, res) => {
+  res.send('hello')
+})
+
 var server = app.listen(3000, () => console.log('Server is listening on port', server.address().port))
