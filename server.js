78d02389613a3a728f6f99cd723703c5e2017d17
Todd Whitelow <todd@whitelow.tech>
2020-09-20 14:26:35 -0600

    create messages aray

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ var app = express()
 
 app.use(express.static(__dirname))
 
+var messages = [
+  {
+    name: "Tim",
+    message: "Hi!"
+  },
+  {
+    name: "Todd",
+    message: "Howdy, Tim!"
+  }
+]
+
 app.get('/messages', (req, res) => {
-  res.send('hello')
+  res.send(messages)
 })
 
 var server = app.listen(3000, () => console.log('Server is listening on port', server.address().port))
